1dc1fd2395190b18d79fdfe01724f53c46bdfba2
monoteist <i.ezerbaev97@gmail.com>
2024-08-27 21:01:35 +0300

    add check subscription for adaptation

--- app/handlers/adaptation.py
+++ app/handlers/adaptation.py
@@ -1,13 +1,18 @@
-from aiogram.fsm.state import StatesGroup, State
+from datetime import datetime
 
+from aiogram.fsm.state import StatesGroup, State
 from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.filters.state import StateFilter
 from aiogram.types import Message
-from app.utils.openai_manager import OpenaiClient
+from sqlalchemy.future import select
 
+from app.utils.openai_manager import OpenaiClient
+from app.database.connect import async_session
+from app.database.models import User
 from config import OPEN_AI_API_TOKEN
+
 
 
 class AdaptationPlanForm(StatesGroup):
@@ -23,13 +28,25 @@ openai_client = OpenaiClient(api_key=OPEN_AI_API_TOKEN)
 
 @router.message(Command("input_employees"))
 async def create_adaptation_plan_command(message: Message, state: FSMContext):
-    await message.answer("Я задам вам 4 вопроса, после чего пришлю готовый план адаптации.")
-    await message.answer("1. Как называется должность, для которой вы составляете план адаптации?")
-    await state.set_state(AdaptationPlanForm.job_title)
+    # Проверка подписки
+    async with async_session() as session:
+        async with session.begin():
+            result = await session.execute(select(User).filter_by(user_id=message.from_user.id))
+            user = result.scalars().first()
+
+            if user and user.subscription_end > datetime.utcnow():
+                await message.answer("Я задам вам 4 вопроса, после чего пришлю готовый план адаптации.")
+                await message.answer("1. Как называется должность, для которой вы составляете план адаптации?")
+                await state.set_state(AdaptationPlanForm.job_title)
+            else:
+                await message.answer("У вас нет активной подписки. Пожалуйста, оформите подписку, чтобы использовать эту функцию.")
 
 
 @router.message(StateFilter(AdaptationPlanForm.job_title))
 async def answer_job_title(message: Message, state: FSMContext):
+    if message.content_type != types.ContentType.TEXT:
+        await message.answer("Пожалуйста, отправьте текстовый ответ.")
+        return
     await state.update_data(job_title=message.text)
     await message.answer("2. Как называется ваша компания?")
     await state.set_state(AdaptationPlanForm.company_name)
@@ -37,6 +54,9 @@ async def answer_job_title(message: Message, state: FSMContext):
 
 @router.message(StateFilter(AdaptationPlanForm.company_name))
 async def answer_company_name(message: Message, state: FSMContext):
+    if message.content_type != types.ContentType.TEXT:
+        await message.answer("Пожалуйста, отправьте текстовый ответ.")
+        return
     await state.update_data(company_name=message.text)
     await message.answer("3. Чем занимается ваша компания?")
     await state.set_state(AdaptationPlanForm.company_activity)
@@ -44,6 +64,9 @@ async def answer_company_name(message: Message, state: FSMContext):
 
 @router.message(StateFilter(AdaptationPlanForm.company_activity))
 async def answer_company_activity(message: Message, state: FSMContext):
+    if message.content_type != types.ContentType.TEXT:
+        await message.answer("Пожалуйста, отправьте текстовый ответ.")
+        return
     await state.update_data(company_activity=message.text)
     await message.answer("4. Какие обязанности у сотрудника на этой должности?")
     await state.set_state(AdaptationPlanForm.job_responsibilities)
@@ -51,6 +74,9 @@ async def answer_company_activity(message: Message, state: FSMContext):
 
 @router.message(StateFilter(AdaptationPlanForm.job_responsibilities))
 async def answer_job_responsibilities(message: Message, state: FSMContext):
+    if message.content_type != types.ContentType.TEXT:
+        await message.answer("Пожалуйста, отправьте текстовый ответ.")
+        return
     await state.update_data(job_responsibilities=message.text)
     data = await state.get_data()
 
@@ -64,6 +90,6 @@ async def answer_job_responsibilities(message: Message, state: FSMContext):
     ]
 
     response = await openai_client.async_get_response(messages)
-    
+
     await message.answer(response.content)
     await state.clear()
