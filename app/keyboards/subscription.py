0faf61e38e1abf9c8f16a1f20a2f7112cc5f65b3
monoteist <i.ezerbaev97@gmail.com>
2024-09-24 12:18:55 +0300

    return real prices

--- app/keyboards/subscription.py
+++ app/keyboards/subscription.py
@@ -6,19 +6,19 @@ def get_subscription_keyboard():
         inline_keyboard=[
             [
                 InlineKeyboardButton(
-                    text="На 1 месяц: 800 рублей",
+                    text="На 1 месяц: 4 990 рублей",
                     callback_data="subscribe_1_month"
                 )
             ],
             [
                 InlineKeyboardButton(
-                    text="На 3 месяца: 900 рублей",
+                    text="На 3 месяца: 11 990 рублей",
                     callback_data="subscribe_3_months"
                 )
             ],
             [
                 InlineKeyboardButton(
-                    text="На год: 990 рублей",
+                    text="На год: 39 990 рублей",
                     callback_data="subscribe_1_year"
                 )
             ]
