4379e59260632fac297b062a38b1ab5209bd59a1
monoteist <i.ezerbaev97@gmail.com>
2024-09-24 12:19:25 +0300

    add logic for update subscribe if user have subscribe before

--- app/utils/db.py
+++ app/utils/db.py
@@ -22,19 +22,30 @@ async def get_user(session: AsyncSession, user_id: int) -> User:
 
 async def update_subscription(session: AsyncSession, user_id: int, days: int):
     """
-    Обновляет или создает дату окончания подписки пользователя.
+    Обновляет или добавляет подписку пользователю, суммируя оставшиеся дни,
+    если подписка еще активна.
 
     :param session: Сессия базы данных.
     :param user_id: ID пользователя в Telegram.
     :param days: Количество дней, на которое продлевается подписка.
     """
     user = await get_user(session, user_id)
+    current_date = datetime.utcnow()
+
     if user:
-        user.subscription_end = datetime.utcnow() + timedelta(days=days)
+        if user.subscription_end and user.subscription_end > current_date:
+            # Если у пользователя активная подписка, суммируем оставшиеся дни
+            remaining_days = (user.subscription_end - current_date).days
+            user.subscription_end = current_date + timedelta(days=days + remaining_days)
+        else:
+            # Если подписки нет или она истекла, устанавливаем новые дни
+            user.subscription_end = current_date + timedelta(days=days)
     else:
+        # Если пользователь новый, создаем его с новой подпиской
         new_user = User(
             user_id=user_id,
-            subscription_end=datetime.utcnow() + timedelta(days=days)
+            subscription_end=current_date + timedelta(days=days)
         )
         session.add(new_user)
+    
     await session.commit()
